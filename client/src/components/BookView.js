9b45d54848e70dc0690c2beec890d16e330a9a70
Ariel Burriss <ariburr@gmail.com>
2018-02-03 20:32:40 -0700

    add book view cover and description

--- client/src/components/BookView.js
+++ client/src/components/BookView.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import noCover from '../assets/default.jpg';
 import { connect } from 'react-redux';
+import {
+  Container,
+  Grid,
+  Image,
+  List,
+ } from 'semantic-ui-react';
 
 class BookView extends React.Component {
 
   render() {
+    const { book } = this.props;
     return (
-      <div>
-        Book View
-      </div>
+      <Container>
+        <Grid>
+          <Grid.Column width={4}>
+            { book.volumeInfo.imageLinks ?
+              <Image
+                src={ book.volumeInfo.imageLinks.thumbnail }
+                alt={`${book.volumeInfo.title} cover`}
+              /> :
+              <Image
+                src={ noCover }
+                alt={`${book.volumeInfo.title} cover`}
+              />
+            }
+            <List>
+              <List.Item>Title: {book.volumeInfo.title}</List.Item>
+              <List.Item>Author: {book.volumeInfo.authors[0]}</List.Item>
+              <List.Item>Pages: {book.volumeInfo.pageCount} pgs</List.Item>
+              <List.Item>Published: {book.volumeInfo.publisher}, {book.volumeInfo.publishedDate}</List.Item>
+            </List>
+          </Grid.Column>
+          <Grid.Column width={12}>
+            { book.volumeInfo.description }
+          </Grid.Column>
+        </Grid>
+      </Container>
     )
   }
 
