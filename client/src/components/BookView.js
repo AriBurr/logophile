c2e0c9ae1a52ab7979c03e5c904ab848e15bffa6
Ariel Burriss <ariburr@gmail.com>
2018-02-03 21:09:57 -0700

    add viewer component and ibsn

--- client/src/components/BookView.js
+++ client/src/components/BookView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import BookViewer from './BookViewer';
 import noCover from '../assets/default.jpg';
 import { connect } from 'react-redux';
 import {
@@ -10,32 +11,48 @@ import {
 
 class BookView extends React.Component {
 
+  getIBSN = (book) => {
+    return book.volumeInfo.industryIdentifiers.map( ibsn => {
+      return (
+        <div>
+          {ibsn.type}: {ibsn.identifier}
+        </div>
+      )
+    })
+  }
+
   render() {
     const { book } = this.props;
     return (
       <Container>
         <Grid>
-          <Grid.Column width={4}>
-            { book.volumeInfo.imageLinks ?
-              <Image
-                src={ book.volumeInfo.imageLinks.thumbnail }
-                alt={`${book.volumeInfo.title} cover`}
-              /> :
-              <Image
-                src={ noCover }
-                alt={`${book.volumeInfo.title} cover`}
-              />
-            }
-            <List>
-              <List.Item>Title: {book.volumeInfo.title}</List.Item>
-              <List.Item>Author: {book.volumeInfo.authors[0]}</List.Item>
-              <List.Item>Pages: {book.volumeInfo.pageCount} pgs</List.Item>
-              <List.Item>Published: {book.volumeInfo.publisher}, {book.volumeInfo.publishedDate}</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={12}>
-            { book.volumeInfo.description }
-          </Grid.Column>
+          <Grid.Row>
+            <Grid.Column width={4}>
+              { book.volumeInfo.imageLinks ?
+                <Image
+                  src={ book.volumeInfo.imageLinks.thumbnail }
+                  alt={`${book.volumeInfo.title} cover`}
+                /> :
+                <Image
+                  src={ noCover }
+                  alt={`${book.volumeInfo.title} cover`}
+                />
+              }
+              <List>
+                <List.Item>Title: {book.volumeInfo.title}</List.Item>
+                <List.Item>Author: {book.volumeInfo.authors[0]}</List.Item>
+                <List.Item>Pages: {book.volumeInfo.pageCount} pgs</List.Item>
+                <List.Item>Published: {book.volumeInfo.publisher}, {book.volumeInfo.publishedDate}</List.Item>
+                <List.Item>{this.getIBSN(book)}</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={12}>
+              { book.volumeInfo.description }
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <BookViewer />
+          </Grid.Row>
         </Grid>
       </Container>
     )
