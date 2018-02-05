3077c93c28d9a06f7a225f7450c0da6749527c54
cgregs32 <cgregs32@gmail.com>
2018-02-05 13:21:55 -0700

    welcome component styled

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Segment, Header, Image } from 'semantic-ui-react';
 import styled from 'styled-components'
 import landing from '../assets/landing.jpg'
+import study from '../assets/study.svg'
+import bookshelf from '../assets/bookshelf.svg'
+import openbook from '../assets/open-book.svg'
 
 const WelcomeWrapper = styled.div`
   width: 100%;
@@ -15,10 +18,34 @@ const WelcomeWrapper = styled.div`
   h1,h2,h3{
     color: white !important;
   }
+  h1{
+    font-size: 40px !important;
+  }
+  p{
+    padding-top: 1%;
+    font-weight: 800;
+  }
+  img{
+    height: 100px;
+    width: 100px;
+    position: center;
+    margin: 0 auto;
+  }
+`
+const LogoContain = styled.div`
+  border: 4px solid white;
+  border-radius: 50%;
+  padding: 2%;
+`
+
+const Logos = styled.div`
+  padding-top: 7%;
+  display: flex;
+  justify-content: space-evenly;
 `
 
 const Transparent = styled.div`
-
+  padding-top: 8%;
   background: rgba(40,75,99, 0.5);
   height: 100vh;
 `
@@ -29,6 +56,20 @@ const Welcome = (props) => {
         <Transparent>
           <Header as='h1'>Join the community of word lovers and book enthusiasts</Header>
           <Header as="h2">Keep track of your book archives </Header>
+          <Logos>
+            <LogoContain>
+              <Image src={bookshelf} alt="logo"></Image>
+              <p>Organize your books</p>
+            </LogoContain>
+            <LogoContain>
+              <Image src={openbook} alt="logo"></Image>
+              <p>Search digital archive</p>
+            </LogoContain>
+            <LogoContain>
+              <Image src={study} alt="logo"></Image>
+              <p>Join the community</p>
+            </LogoContain>
+          </Logos>
         </Transparent>
       </WelcomeWrapper>
   )
