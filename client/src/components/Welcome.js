b363db9fd04e9eab0f38a22c51b9e0143e746a2c
Ariel Burriss <ariburr@gmail.com>
2018-02-09 10:54:32 -0700

    clean up css in Welcome

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -7,44 +7,44 @@ import styled from 'styled-components';
 import { Image } from 'semantic-ui-react';
 
 const LogoContain = styled.div`
-border: 4px solid white;
-border-radius: 50%;
-padding: 2%;
+  border: 4px solid white;
+  border-radius: 50%;
+  padding: 2%;
 `
 const Logos = styled.div`
-padding-top: 7%;
-display: flex;
-justify-content: space-evenly;
+  display: flex;
+  justify-content: space-evenly;
+  padding-top: 7%;
 `
 const Transparent = styled.div`
-padding-top: 8%;
-background: rgba(40,75,99, 0.5);
-height: 100vh;
+  background: rgba(40,75,99, 0.5);
+  height: 100vh;
+  padding-top: 8%;
 `
 const WelcomeWrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-  text-align: center;
-  margin: 0 auto !important;
   background-image: url(${landing});
-  background-size: cover;
   background-position: center;
+  background-size: cover;
   color: white !important;
-  h1,h2,h3 {
-    color: white !important;
-  }
+  height: 100vh;
+  width: 100%;
+  margin: 0 auto !important;
+  text-align: center;
   h1 {
     font-size: 40px;
   }
-  p {
-    padding-top: 1%;
-    font-weight: 800;
+  h1,h2,h3 {
+    color: white;
   }
   img {
     height: 100px;
     width: 100px;
-    position: center;
     margin: 0 auto;
+    position: center;
+  }
+  p {
+    font-weight: 800;
+    padding-top: 1%;
   }
 `
 
