922300177ea237d301a31bdececab66a80a8cfb0
Ariel Burriss <ariburr@gmail.com>
2018-03-03 15:40:41 -0700

    fix typeof error

--- client/src/actions/readings.js
+++ client/src/actions/readings.js
@@ -44,8 +44,11 @@ export const fetchReadings = (id, callback = {}) => {
       .get(`/api/readings?club_id=${id}`, setHeaders())
       .then(res => {
         dispatch({ type: 'GET_READINGS', readings: res.data });
-        callback();
+        if (typeof callback == 'function')
+          callback();
       })
-      .catch(err => dispatch(setFlash('Could not fetch readings!', 'red')));
+      .catch(err => {
+        dispatch(setFlash('Could not fetch readings!', 'red'));
+      });
   };
 };
